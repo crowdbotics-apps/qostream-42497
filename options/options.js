85e00e8390d526dd54127b2c2adb1f875241ed25
Crowdbotics <team@crowdbotics.com>
2023-07-29 01:19:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qostream-42497",
+  url: "https://qostream-42497.botics.co",
+  api: "https://qostream-42497.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
